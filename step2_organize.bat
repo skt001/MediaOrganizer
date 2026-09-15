89611631958694ff02b7bd028d06b70191cc2719
skt001 <48644509+skt001@users.noreply.github.com>
2026-09-15 23:54:59 +0000

    Revert "GeolocLang を OS の LocaleName から取得し、失敗時は en に倒す"
    This reverts commit a14b60ed1cb9bd3c4b257ea029e31748ef75e413.

--- step2_organize.bat
+++ step2_organize.bat
@@ -28,23 +28,6 @@ if not exist "%RULES_VIDEO%" (
     pause & exit /b 1
 )
 
-REM ==== GeolocLang ====
-REM LocaleName の第1サブタグ（ja-JP -> ja）。欠落・不正値は en。
-REM Photos / Movies / NoDate / NoLocation / Unknown は常に英語。
-set "GEO_LANG=en"
-set "LOCALE="
-set "GEO_CAND="
-for /f "tokens=3" %%A in ('reg query "HKCU\Control Panel\International" /v LocaleName 2^>nul ^| find "REG_SZ"') do set "LOCALE=%%A"
-if not defined LOCALE goto :geo_done
-for /f "tokens=1 delims=-" %%A in ("%LOCALE%") do set "GEO_CAND=%%A"
-if not defined GEO_CAND goto :geo_done
-if "%GEO_CAND:~1,1%"=="" goto :geo_done
-if not "%GEO_CAND:~3,1%"=="" goto :geo_done
-echo.%GEO_CAND%| findstr /i /r "^[a-z][a-z]" >nul
-if errorlevel 1 goto :geo_done
-set "GEO_LANG=%GEO_CAND%"
-:geo_done
-
 REM ==== ヘッダー表示 ====
 echo.
 echo ====================================
@@ -52,45 +35,44 @@ echo    Step 2: メディア整理
 echo ====================================
 echo 作業フォルダ: %WORK_DIR%
 echo 開始時刻: %DATE% %TIME%
-echo GeolocLang: %GEO_LANG%
 echo ====================================
 echo.
 
 REM ==== 画像整理 (優先度順: DateTimeOriginal > CreateDate > FileModifyDate > NoDate) ====
-REM GPS があれば country/region/city を追加。無ければ NoLocation/Unknown/Unknown
+REM GPS がある場合は Geolocation で 国/地域/市区 フォルダを追加（無ければ NoLocation）
 echo --- 画像ファイル整理 ---
 echo.
 
 echo [1/8] DateTimeOriginal + Model
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_PHOTO%\p1_datetime_model.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_PHOTO%\p1_datetime_model.args" -r "%UNSORTED%"
 echo.
 
 echo [2/8] DateTimeOriginal + Unknown
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_PHOTO%\p2_datetime_unknown.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_PHOTO%\p2_datetime_unknown.args" -r "%UNSORTED%"
 echo.
 
 echo [3/8] CreateDate + Model
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_PHOTO%\p3_createdate_model.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_PHOTO%\p3_createdate_model.args" -r "%UNSORTED%"
 echo.
 
 echo [4/8] CreateDate + Unknown
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_PHOTO%\p4_createdate_unknown.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_PHOTO%\p4_createdate_unknown.args" -r "%UNSORTED%"
 echo.
 
 echo [5/8] FileModifyDate + Model
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_PHOTO%\p5_filemod_model.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_PHOTO%\p5_filemod_model.args" -r "%UNSORTED%"
 echo.
 
 echo [6/8] FileModifyDate + Unknown
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_PHOTO%\p6_filemod_unknown.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_PHOTO%\p6_filemod_unknown.args" -r "%UNSORTED%"
 echo.
 
 echo [7/8] NoDate + Model
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_PHOTO%\p7_nodate_model.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_PHOTO%\p7_nodate_model.args" -r "%UNSORTED%"
 echo.
 
 echo [8/8] NoDate + Unknown
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_PHOTO%\p8_nodate_unknown.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_PHOTO%\p8_nodate_unknown.args" -r "%UNSORTED%"
 echo.
 
 REM ==== 動画整理 (優先度順: CreateDate > FileModifyDate > NoDate) ====
@@ -98,27 +80,27 @@ echo --- 動画ファイル整理 ---
 echo.
 
 echo [1/6] CreateDate + Make
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_VIDEO%\v1_createdate_make.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_VIDEO%\v1_createdate_make.args" -r "%UNSORTED%"
 echo.
 
 echo [2/6] CreateDate + Unknown
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_VIDEO%\v2_createdate_unknown.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_VIDEO%\v2_createdate_unknown.args" -r "%UNSORTED%"
 echo.
 
 echo [3/6] FileModifyDate + Make
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_VIDEO%\v3_filemod_make.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_VIDEO%\v3_filemod_make.args" -r "%UNSORTED%"
 echo.
 
 echo [4/6] FileModifyDate + Unknown
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_VIDEO%\v4_filemod_unknown.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_VIDEO%\v4_filemod_unknown.args" -r "%UNSORTED%"
 echo.
 
 echo [5/6] NoDate + Make
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_VIDEO%\v5_nodate_make.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_VIDEO%\v5_nodate_make.args" -r "%UNSORTED%"
 echo.
 
 echo [6/6] NoDate + Unknown
-"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=%GEO_LANG% -@ "%RULES_VIDEO%\v6_nodate_unknown.args" -r "%UNSORTED%"
+"%EXIFTOOL%" -api QuickTimeUTC=1 -api geolocation -api GeolocLang=ja -@ "%RULES_VIDEO%\v6_nodate_unknown.args" -r "%UNSORTED%"
 echo.
 
 REM ==== フッター表示 ====
